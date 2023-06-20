837fb3b359d53d7329e55c399bea82be08ef12e3
Kingbossgodora <shreyas13venkatesh@gmail.com>
2023-06-20 13:45:37 +0530

    Update show_grid.py

--- show_grid.py
+++ show_grid.py
@@ -16,17 +16,22 @@ mean_x = x_coords.mean()
 mean_y = y_coords.mean()
 candidate_point = [mean_x, mean_y]
 
+# Adds n candidates each a set distance from the centroid
 for i in range(n_candidates):
-    theta = np.random.uniform(0, 2*np.pi)
-    D = i*(10/(n_candidates - 1))
-    candidate_i = [mean_x + D*np.cos(theta), mean_y + D*np.sin(theta)]
+    theta = np.random.uniform(0, 2 * np.pi)
+    D = i * (10 / (n_candidates - 1))
+    candidate_i = [mean_x + D * np.cos(theta), mean_y + D * np.sin(theta)]
     candidates.update({i: candidate_i})
 
+
 def Distance(a, b):
-    return np.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+    return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+
 
 ballot_box = {}
 
+# Creates a Ballot for each voter that contains their ordered ranking of candidates. The Ballot Box is
+# a dictionary containing both the coordinates of a given voter and their ballot.
 for i in range(len(coordinates)):
     ballot = list(range(n_candidates))
     ballot_new = []
@@ -34,10 +39,11 @@ for i in range(len(coordinates)):
         ballot_new.append(Distance(candidates[j], coordinates[i]))
     ballot_fin = np.column_stack((ballot, ballot_new))
     ballot_fin_n = np.argsort(ballot_fin, axis=0)
-    ballot_box.update({i: {"coordinates": coordinates[i], "vote": ballot_fin[ballot_fin_n[:,1]]}})
+    ballot_box.update({i: {"coordinates": coordinates[i], "vote": ballot_fin[ballot_fin_n[:, 1]]}})
 
 # print(ballot_box[3]["vote"])
 
+# This is my braindead way of getting candidate coordinates into an array so I can plot them
 candidate_array = np.array(list(candidates.values()))
 
 # Calculate distances from each point to the candidate point
@@ -45,7 +51,7 @@ candidate_array = np.array(list(candidates.values()))
 
 # Plot the points and their distances
 plt.scatter(x_coords, y_coords)
-plt.scatter(candidate_array[:,0], candidate_array[:,1], marker='o', color='red', label='Candidate')
+plt.scatter(candidate_array[:, 0], candidate_array[:, 1], marker='o', color='red', label='Candidate')
 plt.legend()
 plt.xlabel('X')
 plt.ylabel('Y')

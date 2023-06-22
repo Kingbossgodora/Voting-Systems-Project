453102100f9e067a8c024832b929d65a43f12cd4
martinaluyten <124562606+martinaluyten@users.noreply.github.com>
2023-06-22 15:32:48 +0200

    borda count function

--- Utils/bordaCountFunction.py
+++ Utils/bordaCountFunction.py
@@ -12,8 +12,9 @@ def bordaCount(ballot_box):
                 candidate_points[candidate_index] = 0
             candidate_points[candidate_index] += points
 
-    candidate_points_list = list(candidate_points.items())
-    candidate_points_list = sorted(candidate_points_list, key=lambda x: x[1], reverse=True)
+    # if output list & not dic
+    #candidate_points_list = list(candidate_points.items())
+    #candidate_points_list = sorted(candidate_points_list, key=lambda x: x[1], reverse=True)
     # sorted_candidate_indices = [candidate_index for candidate_index, _ in candidate_points_list]
-    return candidate_points_list
+    return candidate_points
 

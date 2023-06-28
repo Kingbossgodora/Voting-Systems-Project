3e0628389d589d81a92e0717b85f01c83f29ad80
Kingbossgodora <shreyas13venkatesh@gmail.com>
2023-06-28 19:13:28 +0530

    Update simulations.py

--- Utils/simulations.py
+++ Utils/simulations.py
@@ -8,11 +8,26 @@ from Utils.borda_count import bordaCount
 from Utils.condorcet import condorcet
 from Utils.scoring import scoring
 from Utils.Dictatorship import dictatorship
+from Utils.Stability import stability
 
 
 def voters(n_points, x_range, y_range):
     x_coords = np.random.uniform(x_range[0], x_range[1], n_points)
     y_coords = np.random.uniform(y_range[0], y_range[1], n_points)
+    return np.column_stack((x_coords, y_coords))
+
+
+def voters_normal(n_points, mean, stdev):
+    x_coords = np.random.normal(mean[0], stdev[0], n_points)
+    y_coords = np.random.normal(mean[1], stdev[1], n_points)
+    return np.column_stack((x_coords, y_coords))
+
+
+def voters_bimodal(n_points, mean_1, mean_2, stdev_1, stdev_2):
+    x_coords = np.random.normal(mean_1[0], stdev_1[0], n_points/2)
+    y_coords = np.random.normal(mean_1[1], stdev_1[1], n_points/2)
+    x_coords = np.append(x_coords, np.random.normal(mean_2[0], stdev_2[0], n_points/2))
+    y_coords = np.append(y_coords, np.random.normal(mean_2[1], stdev_2[1], n_points / 2))
     return np.column_stack((x_coords, y_coords))
 
 
@@ -32,12 +47,26 @@ def Candidates(voters_coords, n_candidates):
     return candidates
 
 
+def Candidates_rand(n_candidates, x_range, y_range):
+    candidates = {}
+    for i in range(n_candidates):
+        candidate_i = [np.random.uniform(x_range[0], x_range[1]), np.random.uniform(y_range[0], y_range[1])]
+        candidates.update({i: candidate_i})
+    return candidates
+
+def Candidates_norm(n_candidates, mean, stdev):
+    candidates = {}
+    for i in range(n_candidates):
+        candidate_i = [np.random.normal(mean[0], stdev[0]), np.random.normal(mean[1], stdev[1])]
+        candidates.update({i: candidate_i})
+    return candidates
+
 def simulation(voters_coords, n_candidates, repeats, voting_systems):
     results = []
     ideal_candidate = [voters_coords[:, 0].mean(), voters_coords[:, 1].mean()]
 
     for repeat in range(repeats):
-        candidates = Candidates(voters_coords, n_candidates)
+        candidates = Candidates_rand(n_candidates, (-10,10),(-10,10))
 
         ballot_box = {}
         for i in range(len(voters_coords)):
@@ -50,30 +79,21 @@ def simulation(voters_coords, n_candidates, repeats, voting_systems):
             ballot_box.update({i: {"coordinates": voters_coords[i], "vote": ballot_fin[ballot_fin_n[:, 1]]}})
 
         data = {}
-        avg_discrepancy = {}
         for votingSystem in voting_systems:
             winners = list(votingSystem(ballot_box, n_candidates).keys())
             winner = winners[0]
             discrepancy_val = discrepancy(votingSystem, ballot_box, ideal_candidate, candidates, n_candidates)
             data[votingSystem.__name__] = {"Winner": winner, "Discrepancy": discrepancy_val}
-            # average discrepancy
-            if votingSystem not in avg_discrepancy:
-                avg_discrepancy[votingSystem.__name__] = 0
-            avg_discrepancy[votingSystem.__name__] += discrepancy_val
-        results.append(data)
 
-    avg_discrepancy[votingSystem.__name__] /= repeats
-    return results, avg_discrepancy
+        results.append(data)
+    return results
 
 
 votingSystems_list = [plurality, instant_runoff, bordaCount, condorcet, scoring, dictatorship]
-Simulation, avgDiscrepancy = simulation(votersCoords, num_candidates, 3, votingSystems_list)
+Simulation = simulation(votersCoords, num_candidates, 3, votingSystems_list)
 
-for repeat, (result, discrepancy) in enumerate(zip(Simulation, avgDiscrepancy), start=1):
+for repeat, result in enumerate(Simulation, start=1):
     print("Repeat:", repeat)
     for votingSystem, data in result.items():
         print(votingSystem, ":", data, end="\n")
-    print('average discrepancy:', avgDiscrepancy)
     print()
-
-

89b0b4704a48c9ffc878d1f7fe0e646817facd34
Guillaume <65404325+Guillaume-Bgn@users.noreply.github.com>
2023-06-28 15:07:48 +0200

    Update jack_knife.py

--- Utils/jack_knife.py
+++ Utils/jack_knife.py
@@ -1,13 +1,14 @@
 import random
 from math import floor
 
-
 def JackKnife(ballots, percent):
+    temp_ballot = dict(ballots)
     if(percent >= 1 or percent <= 0):
         raise exception("percentage needed in decimal")
+    
     #cut dictionary
     n_cut= floor(len(ballots)*percent)
+    for key in random.sample(temp_ballot.keys(), n_cut):
+        del temp_ballot[key]
     
-    for key in random.sample(ballots.keys(), n_cut):
-        del ballots[key]
     return ballots

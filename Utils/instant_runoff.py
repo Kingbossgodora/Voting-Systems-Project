99ee385f19aca5a36793ec615833fca204806a75
Guillaume <65404325+Guillaume-Bgn@users.noreply.github.com>
2023-06-29 11:42:47 +0200

    Update instant_runoff.py

--- Utils/instant_runoff.py
+++ Utils/instant_runoff.py
@@ -10,17 +10,19 @@ def instant_runoff(ballots, *args):
     # loop until a candidate wins absolute majority
     while wvote <= majority:
         candidate_points.clear()
-        for i in ballots.keys():
-            vote = ballots[i]["vote"][:, 0][0]
-            # skips eliminated candidates and select vote for next one
-            c = 0
-            while vote in losers:
-                vote = ballots[i]["vote"][:, 0][c]
-                c += 1
-            if vote not in candidate_points:
-                candidate_points[vote] = 0
-            candidate_points[vote] += 1
-
+        try:
+            for i in ballots.keys():
+                vote = ballots[i]["vote"][:, 0][0]
+                # skips eliminated candidates and select vote for next one
+                c = 0
+                while vote in losers:
+                    vote = ballots[i]["vote"][:, 0][c]
+                    c += 1
+                if vote not in candidate_points:
+                    candidate_points[vote] = 0
+                candidate_points[vote] += 1
+        except:
+            continue
         # add the least preferred candidate to the eliminated list
         loser = min(zip(candidate_points.values(), candidate_points.keys()))[1]
         losers.append(loser)
